9dc0ef7ee4f6cb0ac966b58a7c97c0d81d385027
Cerlin <yilindeng1@gmail.com>
2025-12-01 20:35:04 +1100

    terraform provider

---
+++ infra/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.14.0, < 2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.45"  # 允许 5.45.x 及以上 5.x 版本
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      ManagedBy   = "Terraform"
+    }
+  }
+}
